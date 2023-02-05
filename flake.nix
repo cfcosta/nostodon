ae81d37291445f0e34548f2d4c55b3701e9633fb
Cainã Costa <me@cfcosta.com>
2023-02-05 20:25:07 -0300

    feat: nix + direnv

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "An agent to sync mastodon posts to nostr";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ rust-overlay.overlays.default ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            cargo-watch
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" "clippy" "rustfmt" "rust-analyzer" ];
+            })
+          ];
+        };
+      });
+}
