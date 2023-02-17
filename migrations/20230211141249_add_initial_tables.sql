fe893874a3e2444a5ed0bd22c529324eda1d6517
Cainã Costa <me@cfcosta.com>
2023-02-17 09:56:54 -0300

    feat: add timestamps to tables

--- migrations/20230211141249_add_initial_tables.sql
+++ migrations/20230211141249_add_initial_tables.sql
@@ -1,20 +1,35 @@
 create extension if not exists "uuid-ossp";
 
+create function fill_updated_at_on_update()
+returns trigger as $$
+begin
+    new.updated_at = now();
+    return new;
+end;
+$$ language 'plpgsql';
+
 create table users (
     id uuid primary key default uuid_generate_v4(),
     instance_id uuid not null,
     nostr_public_key text not null,
     nostr_private_key text not null,
-    mastodon_user text not null
+    mastodon_user text not null,
+    created_at timestamptz not null default now(),
+    updated_at timestamptz not null default now()
 );
 
 create unique index if not exists users_nostr_public_key_unique_idx on users (nostr_public_key);
 create unique index if not exists users_mastodon_user_unique_idx on users (mastodon_user);
+create trigger fill_users_updated_at_on_update before update on users for each row execute procedure fill_updated_at_on_update();
 
 create table user_blacklists (
     id uuid primary key default uuid_generate_v4(),
-    user_id uuid not null
+    user_id uuid not null,
+    created_at timestamptz not null default now(),
+    updated_at timestamptz not null default now()
 );
+
+create trigger fill_user_blacklists_updated_at_on_update before update on user_blacklists for each row execute procedure fill_updated_at_on_update();
 
 create table profiles (
     id uuid primary key default uuid_generate_v4(),
@@ -24,10 +39,13 @@ create table profiles (
     display_name text not null,
     about text not null,
     picture text not null,
-    nip05 text not null
+    nip05 text not null,
+    created_at timestamptz not null default now(),
+    updated_at timestamptz not null default now()
 );
 
 create unique index if not exists profiles_user_id_unique_idx on profiles (user_id);
+create trigger fill_profiles_updated_at_on_update before update on profiles for each row execute procedure fill_updated_at_on_update();
 
 create type mastodon_post_status as enum('posted', 'deleted');
 
@@ -37,18 +55,24 @@ create table mastodon_posts (
     user_id uuid not null,
     mastodon_id text not null,
     nostr_id text not null,
-    status mastodon_post_status not null
+    status mastodon_post_status not null,
+    created_at timestamptz not null default now(),
+    updated_at timestamptz not null default now()
 );
 
 
 create unique index if not exists mastodon_posts_mastodon_id_unique_idx on mastodon_posts (mastodon_id);
 create unique index if not exists mastodon_posts_nostr_id_unique_idx on mastodon_posts (nostr_id);
+create trigger fill_mastodon_posts_updated_at_on_update before update on mastodon_posts for each row execute procedure fill_updated_at_on_update();
 
 create table mastodon_instances (
     id uuid primary key default uuid_generate_v4(),
     url text not null,
-    blacklisted boolean not null default false
+    blacklisted boolean not null default false,
+    created_at timestamptz not null default now(),
+    updated_at timestamptz not null default now()
 );
 
 create unique index if not exists mastodon_instances_url_unique_idx on mastodon_instances (url);
 create index mastodon_isntances_blacklisted_idx on mastodon_instances (blacklisted);
+create trigger fill_mastodon_instances_updated_at_on_update before update on mastodon_instances for each row execute procedure fill_updated_at_on_update();
--- migrations/20230215234257_add_mastodon_servers.sql
+++ migrations/20230215234257_add_mastodon_servers.sql
@@ -4,7 +4,10 @@ create table mastodon_servers (
   client_key text not null,
   client_secret text not null,
   token text not null,
-  redirect_url text not null default 'urn:ietf:wg:oauth:2.0:oob'
+  redirect_url text not null default 'urn:ietf:wg:oauth:2.0:oob',
+  created_at timestamptz not null default now(),
+  updated_at timestamptz not null default now()
 );
 
 create unique index if not exists mastodon_servers_client_key_unique_idx on mastodon_servers (client_key);
+create trigger fill_mastodon_servers_updated_at_on_update before update on mastodon_servers for each row execute procedure fill_updated_at_on_update();
